8701775125ec94d90756d7c41b7559aa2eb15d6a
Kaustubh Umalkar <kaustubh.umalkar9175@gmail.com>
2026-03-16 16:27:42 +0530

    Add health check route

--- server/server.js
+++ server/server.js
@@ -7,12 +7,19 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'SaaS Dashboard API is running' });
+});
+
 // Routes
 app.use('/api/auth',     require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/activity', require('./routes/activity'));
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT, () => console.log('Server running')))
   .catch(err => console.error(err));
+
